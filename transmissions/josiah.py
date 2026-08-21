5781359870780eeb6eabd20bc27f2a6e3baa199f
Josiah Duvalian <jduvalian@harding.edu>
2026-08-21 18:40:48 -0500

    Add josiah.py introduction

---
+++ transmissions/josiah.py
@@ -0,0 +1,22 @@
+# transmissions/josiah.py
+#
+# A brief introduction to the crew. (Assignment 1, Part B)
+
+
+def main():
+    """Print a short introduction to the crew."""
+    print("Name:    Josiah Duvalian")
+    print("Fact:    Co-owns a Minecraft manhunt server and develops plugins for it.")
+    print(
+        "Problem: Intelligent matchmaking for manhunt queues and last line of defense for an antiswear."
+    )
+    print()
+    print("Discord:               Jonomore (includejodrummer.h)")
+    print("Other CS classes:      Functional/Generic Programming")
+    print("                       AI: Knowledge, Search, and Reasoning")
+    print("Programming languages: C++ (first), Java, Python (duh), HTML/CSS/JS/PHP")
+
+
+# Run main() only when this file is executed directly, not when it's imported.
+if __name__ == "__main__":
+    main()
