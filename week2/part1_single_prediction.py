21c8a57c851039d16bd4a8485e2fff9a8bccfaa9
Joshua Hopper <littlerugaard@gmail.com>
2026-08-27 17:11:45 -0500

    Added comment text block that was forgotten.

--- week2/part1_single_prediction.py
+++ week2/part1_single_prediction.py
@@ -2,6 +2,10 @@
 blade_angle = [8.5, 9.5, 9.9, 9.0]
 weight = 0.5
 
+# Setting the weight to 0.2 over 0.5 would lower each of the values within blade_angle,
+# since it is the only modifier here with the initial values. If it were a negative value,
+# it would flip the sign of the input as well, sending it in the opposite direction.
+
 def neural_network(input, weight, debug = False):
     """
     Inputs: input, weight
