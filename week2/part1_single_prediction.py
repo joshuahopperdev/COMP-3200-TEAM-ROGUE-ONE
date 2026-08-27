469c9f96ee83069c8bb9aaa7d5639f419dad2391
Joshua Hopper <littlerugaard@gmail.com>
2026-08-27 10:45:56 -0500

    Implemented neural_network function

--- week2/part1_single_prediction.py
+++ week2/part1_single_prediction.py
@@ -2,4 +2,17 @@
 blade_angle = [8.5, 9.5, 9.9, 9.0]
 weight = 0.5
 
-# TODO: write neural_network(input, weight) here -- pure Python, no imports.
+def neural_network(input, weight, debug = False):
+    """
+    Inputs: input, weight
+    Loop through input array, and multiply the weight passed in.
+    """
+    prediction = []
+    for i in input:
+        prediction.append(i * weight)
+
+    if debug:
+        print(prediction)
+    return prediction
+
+neural_network(blade_angle, weight, debug = True)
