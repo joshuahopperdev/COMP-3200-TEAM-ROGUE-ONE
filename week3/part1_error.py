dd3a0a645bc8909f6de3fc9ad9f0fd3b0321e05e
Joshua Hopper <littlerugaard@gmail.com>
2026-09-01 09:24:37 -0500

    Revert "Initial Commit For Week 3"
    This reverts commit ca7c55ad8a941ce071019c99145385d25d9fb6b0.

--- week3/part1_error.py
+++
@@ -1,6 +0,0 @@
-# week3/part1_error.py
-balance = [0.65, 0.80, 0.80, 0.90]
-clean = [1, 1, 0, 1]
-weight = 0.5
-# TODO: write squared_error(prediction, goal) -- pure Python first.
-# Then compute predictions and errors for all four sensings.
--- week3/part2_gradient_descent.py
+++
@@ -1,7 +0,0 @@
-balance = [0.65, 0.80, 0.80, 0.90]
-clean = [1, 1, 0, 1]
-# Train on sensing 0: input = 0.65, goal = 1
-weight = 0.5 # starting weight
-# TODO: write gradient_descent(input, goal, weight, iterations)
-# Print error and prediction at each step
-# The loop: pred, error, delta, weight_delta, weight update
