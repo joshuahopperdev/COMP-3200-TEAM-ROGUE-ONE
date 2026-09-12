773de8cb8af575db5fa0605eabe68fe30bee259c
Joshua Hopper <littlerugaard@gmail.com>
2026-09-12 12:30:10 -0500

    Finished part one of part one.

--- week4/part1_multi_input.py
+++ week4/part1_multi_input.py
@@ -1,1 +1,27 @@
 from helpers import w_sum, vect_mat_mul
+
+def ele_mul(scalar, vector, debug = False):
+    """
+    Element wise multiplication.
+    """
+    output = [0] * len(vector)
+    if debug:
+        print(f"Length of vector: {len(vector)}")
+    for i in range(len(vector)):
+        output[i] = scalar *  vector[i]
+
+    return output
+
+def main():
+    """
+    Main function for testing functions.
+    """
+
+    # Example from class
+    delta = -0.14
+    input = [8.5, 0.65, 1.2]
+    weight_deltas = ele_mul(delta, input)
+    print(weight_deltas)
+
+if __name__ == "__main__":
+    main()
