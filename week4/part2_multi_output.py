cccc90e6057dff6661dcbb6b44ae9b1c4cb20d93
Joshua Hopper <littlerugaard@gmail.com>
2026-09-13 14:17:07 -0500

    Added MSE and finished comment block

--- week4/part2_multi_output.py
+++ week4/part2_multi_output.py
@@ -13,17 +13,18 @@ def gradient_descent_outputs(input, weights, trues, alpha, iterations, debug = F
         # Compare and get error
         deltas = [preds[i] - trues[i] for i in range(len(weights))]
         errors = [d ** 2 for d in deltas]
-        error_result.append(errors)
+        mse = sum(errors) / len(errors)
+        error_result.append(mse)
 
         # Learn
         weighted_deltas = [d * input for d in deltas]
         for i in range(len(weights)):
             weights[i] -= alpha * weighted_deltas[i]
 
-        weight_hist_result.append(weights)
+        weight_hist_result.append(weights.copy())
 
         if debug:
-            print(f"Iter: {iter+1}\tPred: {preds}\n\tError: {errors}\n\tWeights: {weights}")
+            print(f"Iter: {iter+1}\tPred: {preds}\n\tError: {errors}\n\tWeights: {weights}\n\tMSE: {mse}")
 
     return weights, error_result, weight_hist_result
 
@@ -41,14 +42,15 @@ def gradient_descent_outputs_numpy(input, weights, trues, alpha, iterations, deb
         # Compare and get error
         deltas = preds - trues
         errors = [d ** 2 for d in deltas]
-        error_result.append(errors)
+        mse = np.mean(errors)
+        error_result.append(mse)
 
         # Learn
         weights -= alpha * deltas * input
-        weight_hist_result.append(weights)
+        weight_hist_result.append(weights.copy())
 
         if debug:
-            print(f"Iter: {iter+1}\tPred: {preds}\n\tError: {errors}\n\tWeights: {weights}")
+            print(f"Iter: {iter+1}\tPred: {preds}\n\tError: {errors}\n\tWeights: {weights}\n\tMSE: {mse}")
 
     return weights, error_result, weight_hist_result
 
@@ -82,7 +84,16 @@ def main():
     # Iter: 1  Error: [0.038025,             0.7569,              0.3422250000000001]
     # Iter: 10 Error: [0.017482684272196927, 0.34799852006905607, 0.15734415844977237]
     # Difference       0.021~                0.409~               0.185~
+    # Iter 1:  MSE: 0.37905000000000005
+    # Iter 10: MSE: 0.17427512093034178
+    # Difference    0.205~
     # The error(delta^2) is much lower on the first. Still quite large on the 2nd, and somewhere in between on the 3rd.
+    # The error is highest on the 2nd one would likely be from our trues being 0, 1, 0. 
+    # Though my guess is that by looking at the difference and the initial, the factor is the same, just the middle number
+    # is a larger amount from a strict number value amount.
+    # The 1st ended up being closest since it has a low error, but this isn't converging fast and it did start close already.
+    # It's just tuning based on one input. It effectively is learning that input and wouldn't be very useful outside of that
+    # one value.
 
 if __name__ == "__main__":
     main()
