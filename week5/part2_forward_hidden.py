5cfce20be4b97fbb254ce50bea8969d66ab1bf50
Williams-Nathanael <nwilliams4@harding.edu>
2026-09-15 15:13:48 -0500

    Completed part 2 comments Still needs final/assignment comments and a bit more testing

--- week5/part2_forward_hidden.py
+++ week5/part2_forward_hidden.py
@@ -15,15 +15,28 @@ weights_0_1 = 2 * np.random.random((3, hidden_size)) - 1 # 3 x 4
 weights_1_2 = 2 * np.random.random((hidden_size, 1)) - 1 # 4 x 1
 
 
-# for use on vectors only!
+# only really designed for numpy vectors, but it might in theory work on other
+# things? For all elements, multiplies them by the truth value of "is it greater
+# than 0", so multiplies >0 nums by 1, <0 nums by 0. Returns the same vector.
 def relu(x):
     zeroed = x * (x > 0)
-    print(f"zeroed is{zeroed}")
     return zeroed
 
+#results experimentally verified very sloppily by using np.ones for the weights
+# seems to work
 def forward(layer_0, weights_0_1, weights_1_2):
+    # first layer is equal to relu of the product of the transpose of the weight matrix
+    # and the input. So for e.g. our 3x4 weight matrix and our 3x1 input vector
+    # here, we're multiplying a 4x3 matrix by a 3x1, and relu of the dot products 
+    # of each row with the vector are our 4 outputs.
     layer_1 = relu(weights_0_1.T@layer_0)
+
+    # second layer is just product of the transpose of the weight matrix
+    # and the input, no relu; so e.g. our 4x1 weight matrix and our 4x1 input
+    # vector will return their dot product, a scalar.
     layer_2 = weights_1_2.T@layer_1
+
+    # output both
     return layer_1, layer_2
 
 
@@ -33,8 +46,12 @@ def main():
     # but there's literally no point, we don't use strike
     # in this step
     
+    # for each input vector...
     for i in range(len(tells)):
+        # run a forward pass...
         layer_1, layer_2 = forward(tells[i], weights_0_1, weights_1_2)
+
+        # and print it out.
         print(f"For sensing {i}, layer 1's result is {layer_1}, and layer 2's result is {layer_2}\n")
     
 
