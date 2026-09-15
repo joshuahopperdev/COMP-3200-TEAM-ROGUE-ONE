05f0d7bd374f066920d1c3fa1c65bbd46df73384
Williams-Nathanael <nwilliams4@harding.edu>
2026-09-15 15:35:18 -0500

    Renamed part 3 file and added function skeletons

---
+++ week5/part3_one_backprop_step.py
@@ -0,0 +1,14 @@
+from part2_forward_hidden import relu
+from part2_forward_hidden import forward
+
+
+
+def relu2deriv(y):
+    return y > 0
+
+def one_step(layer_0, target, weights_0_1, weights_1_2, alpha):
+
+
+    
+    pass
+    #return updated_weights_0_1, updated_weights_1_2, layer_2, layer_2_error
