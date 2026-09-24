33e95ac957a6d62599456258ba6ee19f3a9d7c46
Williams-Nathanael <nwilliams4@harding.edu>
2026-09-24 16:36:30 -0500

    Part 3 backprop started Pretty sure it's working except for the weight update step, but only time will tell.

--- week6/part3_build_from_diagram.py
+++ week6/part3_build_from_diagram.py
@@ -1,7 +1,21 @@
 import numpy as np
 
+
+# 4 sensings, 3 binary tells each
+tells = np.array([[1, 0, 1], # foot shift, no guard drop, exhale
+[0, 1, 1], # no shift, guard drop, exhale
+[0, 0, 1], # only exhale
+[1, 1, 1]]) # all three (the bluff)
+
+# Ground truth: 1 = strike imminent, 0 = they will hold
+strike = np.array([[1, 1, 0, 0]]).T # column vector, shape (4, 1)
+
+
 def relu(input):
     return input * (input > 0)
+
+def relu2deriv(input):
+    return input > 0
 
 # works on an arbitrary length of net, assuming relu on all but the last step
 # input as row vectors, matrices as input dims x output dims
@@ -27,15 +41,40 @@ def train_from_diagram(tells, strike, alpha, epochs, seed, layer_lens = [3, 8, 4
     # initialize weight matrices, sizes calculated from layer_lens.
     # so for e.g. layer_lens of (3, 4, 1), this would initialize 2
     # matrices, a 3x4 and a 4x1
+    np.random.seed(seed)
     weight_mats = [0] * (len(layer_lens) - 1)
     for i in range(len(layer_lens) - 1):
         weight_mats[i] = 2*np.random.random((layer_lens[i], layer_lens[i+1])) - 1
         print(weight_mats[i])
 
+    # make an empty error history
+    # for each epoch.
     err_hist = [0] * epochs
 
-#    for epoch in range(epochs):
-#        for i in range(len(tells)):
+    # for each epoch...
+    for epoch in range(epochs):
+        # for each tell...
+        for i in range(len(tells)):
+            # run a forward pass, saving all the layers
+            # including layer 0, the input
+            layers = forward(tells[i:i+1], weight_mats)
+            # make an empty deltas list
+            deltas = [0] * len(weight_mats)
+            # set the last delta, since its process is a bit special
+            deltas[-1] = layers[-1] - strike[i:i+1]
+
+            # save our error before we go on, since we only need 
+            # this delta to calculate it
+            err_hist[epoch] = deltas[-1]**2
+
+            # set all the other deltas (1 less iteration than there
+            # are deltas, since we already set the last one)
+            for i in range(len(deltas) - 1):
+                deltas[-i-2] = deltas[-i-1] @ weight_mats[-i-1].T * relu2deriv(layers[-i-2])
+                print(deltas[-i-2])
+
+
+
 
 
 
@@ -43,5 +82,5 @@ def train_from_diagram(tells, strike, alpha, epochs, seed, layer_lens = [3, 8, 4
 
 
 #forward(np.array([[0, 1, 2]]), [np.array([2, 4, 9]), np.array([3])])
-#train_from_diagram(0, 0, 0, 0, 0, layer_lens = [3, 8, 4, 1])
+#train_from_diagram(tells, strike, 0, 2, 4, layer_lens = [3, 2, 1])
 
